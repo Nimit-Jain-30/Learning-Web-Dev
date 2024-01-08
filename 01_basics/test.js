7ee903e4ed50c17d47779ecdc9e86a0c653d1bd0
Nimit-Jain-30 <136095141+Nimit-Jain-30@users.noreply.github.com>
2024-01-08 06:17:23 +0000

    variables

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,15 @@
 console.log("Nimit")
+const accountId = 1584
+let accountEmail = "nimit@gmail.com"
+var accountPassword = "1928"
+accountCity = "Ahmedabad"  // this is valid but not a good way of defining a variable
+
+// accountId  = 6494 --> not allowed
+// variables defined as const can not be changed
+
+/* prefer not to use var */
+accountEmail = "nj@gmail.com"
+accountPassword = "abc"
+accountCity = "Udaipur"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
